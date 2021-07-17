ee9f4a3b0fbe3a48eb09f8c2ef7fab8c8fdd1125
thalisonh <thalison_henrique@hotmail.com>
2021-07-17 10:34:48 -0300

    Delete method

--- backend/src/main/java/com/thalisonmorais/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/thalisonmorais/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.thalisonmorais.dscatalog.dto.CategoryDTO;
 import com.thalisonmorais.dscatalog.entities.Category;
 import com.thalisonmorais.dscatalog.repositories.CategoryRepository;
+import com.thalisonmorais.dscatalog.services.exceptions.DatabaseException;
 import com.thalisonmorais.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -58,6 +61,18 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 } 
