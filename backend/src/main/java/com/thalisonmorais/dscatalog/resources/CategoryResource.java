d2dfe0e7fc735474c182530f99296cc48497dc80
thalisonh <thalison_henrique@hotmail.com>
2021-07-17 10:17:32 -0300

    Put method

--- backend/src/main/java/com/thalisonmorais/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/thalisonmorais/dscatalog/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
 		CategoryDTO dto = service.findById(id);
 		
-		return ResponseEntity.ok().body(dto);	
+		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PostMapping
@@ -45,4 +46,12 @@ public class CategoryResource {
 		
 		return ResponseEntity.created(uri).body(dto);
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+		dto = service.update(id, dto);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+	
 }
--- backend/src/main/java/com/thalisonmorais/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/thalisonmorais/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,14 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.thalisonmorais.dscatalog.services.exceptions.EntityNotFoundException;
+import com.thalisonmorais.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
 	//Qualquer exception do tipo EntityNotFoundException o tratamento é redirecionado para esse metodo
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
