be9172ee15538b9d37350540f24bd8e428d72602
thalisonh <thalison_henrique@hotmail.com>
2021-08-16 22:02:21 -0300

    Custom constraintValidator

--- backend/src/main/java/com/thalisonmorais/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/thalisonmorais/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.thalisonmorais.dscatalog.dto;
 
+import com.thalisonmorais.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
