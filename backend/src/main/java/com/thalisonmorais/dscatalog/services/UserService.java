ad66324ce097cba0526828e7333d08f64a870c0a
thalisonh <thalison_henrique@hotmail.com>
2021-08-16 22:27:22 -0300

    UserUpdateValidator

--- backend/src/main/java/com/thalisonmorais/dscatalog/services/UserService.java
+++ backend/src/main/java/com/thalisonmorais/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.thalisonmorais.dscatalog.dto.RoleDTO;
 import com.thalisonmorais.dscatalog.dto.UserDTO;
 import com.thalisonmorais.dscatalog.dto.UserInsertDTO;
+import com.thalisonmorais.dscatalog.dto.UserUpdateDTO;
 import com.thalisonmorais.dscatalog.entities.Role;
 import com.thalisonmorais.dscatalog.entities.User;
 import com.thalisonmorais.dscatalog.repositories.RoleRepository;
@@ -62,7 +63,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id);
 			copyDtoToEntity(dto, entity);
